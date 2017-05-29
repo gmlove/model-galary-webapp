b540e31b8f5015c4de2ff78e53b2a53fa994db1c
gmlove <gracekinglau@gmail.com>
2017-05-29 18:28:20 +0800

    make model-galary as home page

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -10,23 +10,38 @@
 import React from 'react';
 import Home from './Home';
 import Layout from '../../components/Layout';
+import ModelGalary from '../model-galary/ModelGalary';
 
 export default {
 
   path: '/',
 
-  async action({ fetch }) {
-    const resp = await fetch('/graphql', {
-      body: JSON.stringify({
-        query: '{news{title,link,content}}',
-      }),
-    });
-    const { data } = await resp.json();
-    if (!data || !data.news) throw new Error('Failed to load the news feed.');
-    return {
-      title: 'React Starter Kit',
-      component: <Layout><Home news={data.news} /></Layout>,
-    };
-  },
+  children: [
+    {
+      path: '/',
+      action() {
+        return {
+          title: 'Model Galary',
+          component: <Layout><ModelGalary title="Model Galary" /></Layout>,
+        };
+      },
+    },
+    {
+      path: '/react',
+      async action({ fetch }) {
+        const resp = await fetch('/graphql', {
+          body: JSON.stringify({
+            query: '{news{title,link,content}}',
+          }),
+        });
+        const { data } = await resp.json();
+        if (!data || !data.news) throw new Error('Failed to load the news feed.');
+        return {
+          title: 'React Starter Kit',
+          component: <Layout><Home news={data.news} /></Layout>,
+        };
+      },
+    },
+  ],
 
 };
